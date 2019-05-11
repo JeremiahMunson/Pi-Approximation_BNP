07b429eb24dbc67e46e41bde8076a30284d5d629
Jeremiah Munson <j.munson225@gmail.com>
2019-05-11 12:55:06 -0500

    Removed method to place needle and moved to check cross line. Commented. Variable names make more sense. Changed checking line crossing by finding regions between lines.

--- Grid.h
+++ Grid.h
@@ -1,20 +1,15 @@
-#include <iostream>
-
 using namespace std;
-
 
 class Grid{
 public:
-    Grid(float* sep, float* len);
+    Grid(float* sep, float* len, int* numLines);
     float approximatePi(int trials);
 private:
     float lineSeparation;
     float needleLength;
-    float needleLocation;
-    float needleAngle;
+    int gridSize;
     int numTrials;
     int numCrosses;
-    void createNeedlePlace(float* loc, float* angle);
-    void checkNeedleCrossLine(float* loc, float* angle);
+    void checkNeedleCrossLine();
     double pi = 3.14159265358979323846;
 };
