13b839c1b9d5679354f106e666def887c39459e5
Jeremiah Munson <j.munson225@gmail.com>
2019-05-11 12:34:44 -0500

    Added comments and user inputs number of grid lines to be used.

--- main.cpp
+++ main.cpp
@@ -4,20 +4,30 @@
 using namespace std;
 
 int main(){
+    // Distance separating the drawn lines (arbitrary units)
     float lineSep;
-    float needleLen;
-    int numTrials;
-    char tryAgain;
+    // Length of needle (arbitrary units)
+    float needleLen; 
+    // Number of trials to calculate probability of needle crossing line
+    int numTrials; 
+    // Used to check if user wants to do it again or quit the program
+    char tryAgain; 
+    // Size of the grid. Could be arbitrary but I want users to have 
+    // control, like they are doing it by hand.
+    int numGridLines; 
 
     while(true)
     {
         cout << "Line Separation = ";
         cin >> lineSep;
 
+        cout << "Grid Size (Number of Lines) = ";
+        cin >> numGridLines;
+
         cout << "Needle Length = ";
         cin >> needleLen;
     
-        Grid test = Grid(&lineSep, &needleLen);
+        Grid test = Grid(&lineSep, &needleLen, &numGridLines);
 
         cout << "Number of Trials = ";
         cin >> numTrials;
@@ -28,10 +38,14 @@ int main(){
         cout << "Would you like to try again (y/n)?" << endl;
         cin >> tryAgain;
 
+        // If they say 'y' or 'Y' then they want to keep going.
+        // Otherwise it assumes you don't want to keep going.
         if(tryAgain != 'y' && tryAgain != 'Y')
         {
             break;
         }
-    }
 
+        // Add a blank line between trials
+        cout << endl;
+    }
 }
